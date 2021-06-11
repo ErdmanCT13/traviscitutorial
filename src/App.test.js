ec165f058b365af334a40565427939a37adf1beb
ErdmanCT13 <36122787+ErdmanCT13@users.noreply.github.com>
2021-06-11 00:39:13 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/I made a change. It should be reflected in my live deployment!/i);
   expect(linkElement).toBeInTheDocument();
 });
